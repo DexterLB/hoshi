5a522f7d6f5269dc271df9510a0c25af923a2c6a
dexterlb <dexterlb@qtrp.org>
2019-09-04 11:45:51 +0300

    implement encode / decode

--- js/hoshi/src/types.ts
+++ js/hoshi/src/types.ts
@@ -14,7 +14,7 @@ export interface Schema {
     meta: MetaData,
 }
 
-export type Version = string
+export type Version = "0"
 export type Encoding = "json" // currently "json" is the only valid encoding
 
 /**
@@ -122,54 +122,99 @@ export function is_void(t: Type): t is TVoid {
     return t.kind == 'type-basic' && t.sub == 'void'
 }
 
-/**
- * Check if a Javascript value conforms to the given type
- */
-export function encode(x: Data, s: Schema): string {
+export function encode(x: Data, s: Schema): string | TypeError {
+    let err = (text: string) => {
+        term: x,
+        type: s.t,
+        error: text,
+    }
+
+    if s.version != "0" {
+        return err("unknown schema version");
+    }
+
     switch(s.encoding) {
         case "json":
-            typecheck(x, 
+            let err = typecheck(x, s.t)
+            if err != "ok" {
+                return err
+            }
+            return JSON.stringify(x);
+        default:
+            return err("unknown schema encoding");
     }
 }
 
-function 
+export function decode(data: string, s: Schema): { term: Data } | TypeError {
+    let err = (text: string) => {
+        data: data,
+        type: s.t,
+        error: text,
+    }
+
+    if s.version != "0" {
+        return err("unknown schema version");
+    }
+
+    switch(s.encoding) {
+        case "json":
+            let x = JSON.parse(data)
+            let err = typecheck(x, s.t)
+            if err != "ok" {
+                return err
+            }
+            return JSON.stringify(x);
+        default:
+            return err("unknown schema encoding");
+    }
+}
 
 /**
  * Check if a Javascript value conforms to the given type
  */
-export function typecheck(x: Data, t: Type): boolean {
+export function typecheck(x: Data, t: Type): Ok | TypeError {
+    let check = (ok: boolean) => {
+        if ok {
+            return "ok"
+        }
+        return {
+            error: "cannot coerce term into type",
+            type: t
+        }
+    }
+
     switch(t.kind) {
         case "type-basic": {
             switch(t.sub) {
-                case "void":   return false;
-                case "null":   return (x == null);
-                case "bool":   return (typeof x == "boolean");
-                case "int":    return (typeof x == "number");
-                case "float":  return (typeof x == "number");
-                case "string": return (typeof x == "string");
+                case "void":   return check(false);
+                case "null":   return check(x == null);
+                case "bool":   return check(typeof x == "boolean");
+                case "int":    return check(typeof x == "number");
+                case "float":  return check(typeof x == "number");
+                case "string": return check(typeof x == "string");
             }
-            return false;
+            return check(false);
         }
         case "type-literal": {
-            return JSON.stringify(x) == JSON.stringify(t.value);
+            return check(JSON.stringify(x) == JSON.stringify(t.value));
         }
         case "type-union": {
             for (let alt of t.alts) {
                 if (typecheck(x, alt)) {
-                    return true;
+                    return check(true);
                 }
             }
-            return false;
+            return check(false);
         }
     }
     console.log('please finish the implementation of typecheck')
-    return true
+    return check(true)
 }
 
-export interface EncodeError extends Error {
-     
-}
+type Ok = "ok"
 
-export interface Error {
-    text: string
+export interface TypeError {
+    error: string
+    type: Type
+    term: Data | null
 }
